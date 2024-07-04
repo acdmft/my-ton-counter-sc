6dce347c157814376749e64d1e9643fcd5d84f40
gilert <crankyfox@protonmail.com>
2024-07-04 19:30:40 +0200

    add functions to the contract wrapper

--- wrappers/MainContract.ts
+++ wrappers/MainContract.ts
@@ -4,14 +4,23 @@ const crc32 = require("crc-32");
 export type MainContractConfig = {
     id: number;
     counter: number;
+    owner: Address;
+    // sender: Address;
 };
 
 export function mainContractConfigToCell(config: MainContractConfig): Cell {
-    return beginCell().storeUint(config.id, 32).storeUint(config.counter, 32).endCell();
+    return beginCell()
+        .storeUint(config.id, 32)
+        .storeUint(config.counter, 32)
+        .storeAddress(config.owner)
+        // .storeAddress(config.sender)
+        .endCell();
 }
 
 export const Opcodes = {
     increase: crc32.str('op::increase'), // 0x7e8764ef,
+    deposit: crc32.str('op::deposit') >>> 0, // should be unsigned integer
+    withdraw: crc32.str('op::withdraw') >>>0
 };
 
 export class MainContract implements Contract {
@@ -55,6 +64,45 @@ export class MainContract implements Contract {
         });
     }
 
+    async sendDeposit(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            value: bigint;
+            queryID?: number;
+        }
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.deposit, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .endCell(),
+        });
+    }
+
+    async sendWithdraw(provider: ContractProvider,
+        via: Sender,
+        opts: {
+            value: bigint;
+            queryID?: number;
+            sender: Address;
+            amount: bigint;
+        }
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.withdraw, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .storeAddress(opts.sender)
+                .storeCoins(opts.amount)
+                .endCell(),
+        });
+    }
+
     async getCounter(provider: ContractProvider) {
         const result = await provider.get('get_counter', []);
         return result.stack.readNumber();
@@ -63,5 +111,15 @@ export class MainContract implements Contract {
     async getID(provider: ContractProvider) {
         const result = await provider.get('get_id', []);
         return result.stack.readNumber();
+    }
+
+    async getOwnerAddress(provider: ContractProvider) {
+        const result = await provider.get('get_owner', []);
+        return result.stack.readAddress();
+    }
+
+    async getContractBalance(provider: ContractProvider){
+        const result = await provider.get('get_contract_balance', []);
+        return result.stack.readBigNumber();
     }
 }
